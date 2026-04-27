c348cddc00d5ac517cd9eb9f877219d6ea8805eb
acegikmoop-code <acegikmoop@gmail.com>
2026-04-27 16:04:04 +0900

    ui구현

--- frontend/src/routes/Router.jsx
+++ frontend/src/routes/Router.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom'; 
 import Layout from '../components/layout/Layout';
 import Home from '../pages/Home';
 import Search from '../pages/Search';
@@ -7,15 +7,13 @@ import Schedule from '../pages/Schedule';
 
 export default function AppRouter() {
   return (
-    <Router>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/chat" element={<Chatbot />} />
-          <Route path="/schedule" element={<Schedule />} />
-        </Route>
-      </Routes>
-    </Router>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/chat" element={<Chatbot />} />
+        <Route path="/schedule" element={<Schedule />} />
+      </Route>
+    </Routes>
   );
 }
